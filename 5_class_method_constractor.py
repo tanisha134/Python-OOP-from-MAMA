56d4697c25360ef1c1450719dd979a130a213773
Tanisha <jannatulnurtanisha63@gmail.com>
2025-03-07 16:09:26 +0600

    Gloal variable and Local Varible

--- 5_class_method_constractor.py
+++ 5_class_method_constractor.py
@@ -1,6 +1,9 @@
 class girl:
+    # Global Variable
+    School = 'Ankur School'
     #Constractor
     def __init__(self, name , age, home ):
+        # local Variable
         self.name=name
         self.age=age
         self.home=home
@@ -9,12 +12,14 @@ class girl:
 
     # To show this function
     def show(self):
-        print(f"Name is : {self.name} , Age is : {self.age} , Home is at : {self.home}")
+        print(f"Name is : {self.name} , Age is : {self.age} , Home is at : {self.home} , Schol is : {self.School}")
         
 
 hm=girl('safa', 13 , 'abashik')
 hm.show()
 hm1=girl('soha',14, 'Khulna')
 hm1.show()
+hm2=girl('Tanisha', 11, 'Mugultuli')
+hm2.show()
     
     
